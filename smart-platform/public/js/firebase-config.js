87373520448185313a7da243fb7539f982d88645
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-02-09 16:27:05 +0200

    Update firebase-config.js

--- smart-platform/public/js/firebase-config.js
+++ smart-platform/public/js/firebase-config.js
@@ -1,19 +1,15 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCFVu8FHYq2leGA1F9SQEAXmn1agv1V1cM",
-    authDomain: "smartplatform-513c3.firebaseapp.com",
-    projectId: "smartplatform-513c3",
-    storageBucket: "smartplatform-513c3.firebasestorage.app",
-    messagingSenderId: "906640049959",
-    appId: "1:906640049959:web:c6c619a53ef4d6f9704b02"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCFVu8FHYq2leGA1F9SQEAXmn1agv1V1cM",
+  authDomain: "smartplatform-513c3.firebaseapp.com",
+  projectId: "smartplatform-513c3",
+  storageBucket: "smartplatform-513c3.firebasestorage.app",
+  messagingSenderId: "906640049959",
+  appId: "1:906640049959:web:c6c619a53ef4d6f9704b02"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const app = initializeApp(firebaseConfig);
+
+console.log("🔥 Firebase Connected Successfully");
