2d64788dcfad66ffbd3c3c3f6d9dc0b98a9e12d1
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-02-09 14:56:56 +0200

    Update firebase-config.js

--- smart-platform/public/js/firebase-config.js
+++ smart-platform/public/js/firebase-config.js
@@ -1,15 +1,1 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-
-console.log("🔥 Firebase Connected Successfully");
