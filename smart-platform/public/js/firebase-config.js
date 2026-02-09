35f64d2aa33c17f067b34588e66c7ead001a14cb
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-02-09 14:37:54 +0200

    Update firebase-config.js

--- smart-platform/public/js/firebase-config.js
+++ smart-platform/public/js/firebase-config.js
@@ -1,18 +1,15 @@
-// Import Firebase
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDutDzaVU4GYP0lhMfg0ab5aLb-w-11628",
-  authDomain: "hr-system-d7fc4.firebaseapp.com",
-  projectId: "hr-system-d7fc4",
-  storageBucket: "hr-system-d7fc4.firebasestorage.app",
-  messagingSenderId: "1078587501723",
-  appId: "1:1078587501723:web:88a31496e6f815c0362a3e"
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { auth };
-
+console.log("🔥 Firebase Connected Successfully");
