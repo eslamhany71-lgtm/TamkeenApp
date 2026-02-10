77975ca7d4fa704fce9438560a64943a6bb3f131
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-02-10 15:29:44 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,30 @@
-// التأكد من حالة تسجيل الدخول
 firebase.auth().onAuthStateChanged((user) => {
+    // نعرف إحنا في أنهي صفحة دلوقتي
+    const currentPage = window.location.pathname.split("/").pop();
+
     if (user) {
-        // المستخدم مسجل دخول
-        document.getElementById('userEmail').innerText = "مرحباً: " + user.email;
+        // لو المستخدم مسجل دخول
+        console.log("المستخدم مسجل دخول:", user.email);
+        
+        // لو هو في صفحة الـ login (index.html) نوديه للـ home
+        if (currentPage === "index.html" || currentPage === "") {
+            window.location.href = "home.html";
+        }
+
+        // لو في صفحة الـ home، نعرض الإيميل
+        const userEmailElement = document.getElementById('userEmail');
+        if (userEmailElement) {
+            userEmailElement.innerText = "مرحباً: " + user.email;
+        }
+
     } else {
-        // المستخدم مش مسجل دخول، ابعته لصفحة الـ login
-        window.location.href = "index.html";
+        // لو المستخدم مش مسجل دخول
+        console.log("لا يوجد مستخدم مسجل");
+
+        // لو هو مش في صفحة الـ login، نطرده للـ login
+        if (currentPage !== "index.html" && currentPage !== "") {
+            window.location.href = "index.html";
+        }
     }
 });
 
