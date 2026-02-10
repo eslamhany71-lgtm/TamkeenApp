f3206b47c588420a984bf5ff5299976fde49bf5a
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-02-10 15:25:21 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,19 @@
+// التأكد من حالة تسجيل الدخول
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // المستخدم مسجل دخول
+        document.getElementById('userEmail').innerText = "مرحباً: " + user.email;
+    } else {
+        // المستخدم مش مسجل دخول، ابعته لصفحة الـ login
+        window.location.href = "index.html";
+    }
+});
 
+// دالة تسجيل الخروج
+function logout() {
+    firebase.auth().signOut().then(() => {
+        window.location.href = "index.html";
+    }).catch((error) => {
+        alert("حدث خطأ أثناء تسجيل الخروج");
+    });
+}
