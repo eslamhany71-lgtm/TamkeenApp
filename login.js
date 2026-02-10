88ee889ef568db570a79690a62fa58762878384b
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-02-10 12:30:00 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,14 @@
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(() => {
+            message.innerText = "تم تسجيل الدخول ✅";
+        })
+        .catch(() => {
+            message.innerText = "بيانات الدخول غير صحيحة ❌";
+        });
+}
 
