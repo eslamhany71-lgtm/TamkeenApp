db03e4f614fa405107338d780fe699fb4869d945
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-02-10 10:38:15 +0200

    Update firebase.js

--- smart-platform/public/js/firebase.js
+++ smart-platform/public/js/firebase.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFVu8FHYq2leGA1F9SQEAXmn1agv1V1cM",
+  authDomain: "smartplatform-513c3.firebaseapp.com",
+  projectId: "smartplatform-513c3",
+  storageBucket: "smartplatform-513c3.appspot.com",
+  messagingSenderId: "906640049959",
+  appId: "1:906640049959:web:c6c619a53ef4d6f9704b02"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
