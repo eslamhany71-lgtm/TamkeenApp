aae92c4c8b86cac95465640d3a0e57b1d05bafef
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-02-10 10:53:39 +0200

    Update and rename firebase.js to firebase-config.js

--- smart-platform/public/js/firebase-config.js
+++ smart-platform/public/js/firebase-config.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCFVu8FHYq2leGA1F9SQEAXmn1agv1V1cM",
@@ -10,7 +9,6 @@ const firebaseConfig = {
   appId: "1:906640049959:web:c6c619a53ef4d6f9704b02"
 };
 
-
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 
+console.log("🔥 Firebase connected", app);
